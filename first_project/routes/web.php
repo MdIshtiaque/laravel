e09db6f6a96f528f6986aa7afe91aeb2e45b613b
Md Ishtiaque Ferdous <emon123653@gmail.com>
2022-11-04 03:08:10 +0600

    Constraining Possible Route Parameters Values

--- first_project/routes/web.php
+++ first_project/routes/web.php
@@ -23,5 +23,15 @@ Route::get('/contact-us', function () {
     return view('contact');
 })->name('contact');
 Route::get('/service-us/{service_id}/{service_name?}', function ($service_id, $service_name = null) {
-    return "service"." ".$service_id." ".$service_name;
+    return "service" . " " . $service_id . " " . $service_name;
 })->name('service');
+
+Route::get('/user/{id}/{name}', function ($id, $name) {
+    echo $name . " " . $id;
+})->where(['id' => '[0-9]+', 'name' => '[a-z]+']);
+
+Route::get('/category/{category}', function ($category) {
+    echo $category;
+    //WhereIn -> search in array
+})->whereIn('category', ['electronics', 'movie', 'games', 'books', 'laptops']);
+ 
