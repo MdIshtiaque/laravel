2a7378f738f57c087ea3e7e0a45213a8c7b23f55
Md Ishtiaque Ferdous <emon123653@gmail.com>
2022-11-08 01:29:49 +0600

    Grouping Routes

--- first_project/routes/web.php
+++ first_project/routes/web.php
@@ -14,95 +14,95 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function (Request $request) {
+// Route::get('/', function (Request $request) {
 
-    // dd(
-    //     $request->path(),
-    //     $request->is("/"),
-    //     $request->fullUrl(),
-    //     $request->host(),
-    //     $request->httpHost(),
-    //     $request->schemeAndHttpHost(),
+//     // dd(
+//     //     $request->path(),
+//     //     $request->is("/"),
+//     //     $request->fullUrl(),
+//     //     $request->host(),
+//     //     $request->httpHost(),
+//     //     $request->schemeAndHttpHost(),
 
-    //     $request->routeIs('home'),
-    //     $request->header('X-Header-Name'),
-    //     $request->bearerToken(),
-    //     $request->ip(),
-    //     $request->prefers(['text/html', 'application/json']),
-    // );
-    // $data = [
-    //     'page_name' => 'Home Page',
-    //     'name' => 'Laravel 9 course '
-    // ];
-    // return response($data)->header('content-type', 'application/json')
-    // ->cookie('Md Emon', 'Laravel 9 course', 3600);
-    //return redirect('/contact-us');
+//     //     $request->routeIs('home'),
+//     //     $request->header('X-Header-Name'),
+//     //     $request->bearerToken(),
+//     //     $request->ip(),
+//     //     $request->prefers(['text/html', 'application/json']),
+//     // );
+//     // $data = [
+//     //     'page_name' => 'Home Page',
+//     //     'name' => 'Laravel 9 course '
+//     // ];
+//     // return response($data)->header('content-type', 'application/json')
+//     // ->cookie('Md Emon', 'Laravel 9 course', 3600);
+//     //return redirect('/contact-us');
 
-    //passing data from backend to frontend(array, compact)
-    return view('home', [
-        'page_name' => 'Home Page',
-        'name' => 'Laravel 9 course '
-    ]);
-})->name('home');
+//     //passing data from backend to frontend(array, compact)
+//     return view('home', [
+//         'page_name' => 'Home Page',
+//         'name' => 'Laravel 9 course '
+//     ]);
+// })->name('home');
 
-Route::get('/about-us', function () {
-    return view('about', [
-        'page_name' => 'About Page'
-    ]);
-})->name('about');
+// Route::get('/about-us', function () {
+//     return view('about', [
+//         'page_name' => 'About Page'
+//     ]);
+// })->name('about');
 
-Route::get('/contact-us', function () {
-    return view('contact', [
-        'page_name' => 'Contact Page',
-        'mobile' => "+8801797734294"
-    ]);
-})->name('contact');
+// Route::get('/contact-us', function () {
+//     return view('contact', [
+//         'page_name' => 'Contact Page',
+//         'mobile' => "+8801797734294"
+//     ]);
+// })->name('contact');
 
-Route::get('/service-us', function () {
-    $page_name = "Service Page";
-    $services = [
-        'Web Design',
-        'Web Development',
-        'SEO',
-        'Digital Marketing',
-        'Graphics Design'
-    ];
-    $product = [
-        1 => [
-            'name' => 'Bag',
-            'color' => 'Red',
-            'price' => 1200,
-        ],
-        2 => [
-            'name' => 'Shoes',
-            'color' => 'Black',
-            'price' => 200,
-        ],
-        3 => [
-            'name' => 'Mouse',
-            'color' => 'Black',
-            'price' => 2500,
-        ],
-    ];
-    $product_count = count($product);
+// Route::get('/service-us', function () {
+//     $page_name = "Service Page";
+//     $services = [
+//         'Web Design',
+//         'Web Development',
+//         'SEO',
+//         'Digital Marketing',
+//         'Graphics Design'
+//     ];
+//     $product = [
+//         1 => [
+//             'name' => 'Bag',
+//             'color' => 'Red',
+//             'price' => 1200,
+//         ],
+//         2 => [
+//             'name' => 'Shoes',
+//             'color' => 'Black',
+//             'price' => 200,
+//         ],
+//         3 => [
+//             'name' => 'Mouse',
+//             'color' => 'Black',
+//             'price' => 2500,
+//         ],
+//     ];
+//     $product_count = count($product);
 
-    $product_count = count($product);
+//     $product_count = count($product);
 
-    //return json
-    // return response()->json([
-    //     'product' => $product,
-    //     'product count' => $product_count,
+//     //return json
+//     // return response()->json([
+//     //     'product' => $product,
+//     //     'product count' => $product_count,
 
-    // ]);
+//     // ]);
 
 
-    return view('service', compact(
-        'page_name',
-        'services',
-        'product_count',
-        'product'
-    ));
-})->name('service');
+//     return view('service', compact(
+//         'page_name',
+//         'services',
+//         'product_count',
+//         'product'
+//     ));
+// })->name('service');
 
 
 
@@ -134,3 +134,65 @@ Route::get('/resume/download', function () {
         "My CV for Laravel 9.pdf"
     );
 })->name('resume');
+
+
+
+// Grouping Routes
+Route::prefix('page')->name('')->group(function(){
+    Route::get('/home', function(){
+        return view('home', [
+            'page_name' => 'Home Page',
+            'name' => 'Laravel 9 course '
+        ]);
+    })->name('home');
+
+    Route::get('/contact', function(){
+        return view('contact', [
+            'page_name' => 'Contact Page',
+            'mobile' => "+8801797734294"
+        ]);
+    })->name('contact');
+
+    Route::get('/service', function(){
+        $page_name = "Service Page";
+        $services = [
+            'Web Design',
+            'Web Development',
+            'SEO',
+            'Digital Marketing',
+            'Graphics Design'
+        ];
+        $product = [
+            1 => [
+                'name' => 'Bag',
+                'color' => 'Red',
+                'price' => 1200,
+            ],
+            2 => [
+                'name' => 'Shoes',
+                'color' => 'Black',
+                'price' => 200,
+            ],
+            3 => [
+                'name' => 'Mouse',
+                'color' => 'Black',
+                'price' => 2500,
+            ],
+        ];
+        $product_count = count($product);
+
+        $product_count = count($product);
+        return view('service', compact(
+            'page_name',
+            'services',
+            'product_count',
+            'product'
+        ));
+    })->name('service');
+
+    Route::get('/about', function(){
+        return view('about', [
+            'page_name' => 'About Page'
+        ]);
+    })->name('about');
+});
